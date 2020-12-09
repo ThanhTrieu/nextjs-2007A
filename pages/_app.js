d4fe16f53fd8495c566cff152c573bd3d791d646
Nguyen Thanh Trieu <trieunguyenthanh@admicro.vn>
2020-12-09 10:01:59 +0700

    update code

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,20 @@ import 'antd/dist/antd.css'
 import '../styles/globals.css'
 
 import { Provider } from 'react-redux'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
 import { wrapper, configStore } from '../redux/configStore'
+import { Skeleton } from 'antd'
 
 
 function MyApp({ Component, pageProps }) {
   const store = configStore(pageProps.initialReduxState)
+  const persistor = persistStore(store)
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <PersistGate loading={<Skeleton/>} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   )
 }
