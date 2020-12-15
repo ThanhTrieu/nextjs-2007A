964bce6b8b1a5d30fc1834e9ea8d448bbc65b66b
Nguyen Thanh Trieu <trieunguyenthanh@admicro.vn>
2020-12-15 15:18:51 +0700

    update code

---
+++ pages/testv1/[...movie].js
@@ -0,0 +1,41 @@
+import { useRouter } from 'next/router'
+import axios from 'axios'
+
+export default function DetailMovie({movie}) {
+  const router = useRouter()
+  const { id } = router.query
+  console.log(movie)
+  return (
+    <>
+      <h1>ID phim : {id}</h1>
+    </>
+  )
+}
+
+export const getDataMoviesById = async (id = 0) => {
+  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=0aecc06bb4fadb06b5f071fef0c2ce6d&language=vi&append_to_response=videos,images&include_image_language=vi,null`;
+  const response = await axios.get(url);
+  const result = await response.status === 200 ? response.data : {};
+  return result;
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { movie: ["*"] } },
+    ],
+    fallback: true
+  };
+}
+
+export async function getStaticProps( context ) {
+  const { params = [] } =  context || {}
+  const arrParams = params.movie || []
+  const id = arrParams[1]
+  const result = await getDataMoviesById(id)
+  return {
+    props: {
+      movie: result,
+    },
+  }
+}
